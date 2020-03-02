ed8c9dc850447500a53c9b4f1631024c6858bbaf
Mark Stabler <mark@stablerwritingservices.com>
2020-03-02 15:39:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,13 @@
 #write your code here
-
 def countdown(n)
+  while n > 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
   while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
